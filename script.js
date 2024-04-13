57bb8d8bebe9f44639ceb88eba0a2b52dd650a96
Prietolaura <166888951+Prietolaura@users.noreply.github.com>
2024-04-12 22:10:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let mostrador = document.getElementById("mostrador");
+let seleccion = document.getElementById("seleccion");
+let imgSeleccionada = document.getElementById("img");
+let modeloSeleccionado = document.getElementById("modelo");
+let descripSeleccionada = document.getElementById("descripcion");
+let precioSeleccionado = document.getElementById("precio");
+
+function cargar(item){
+    quitarBordes();
+    mostrador.style.width = "60%";
+    seleccion.style.width = "40%";
+    seleccion.style.opacity = "1";
+    item.style.border = "2px solid red";
+
+    imgSeleccionada.src = item.getElementsByTagName("img")[0].src;
+
+    modeloSeleccionado.innerHTML =  item.getElementsByTagName("p")[0].innerHTML;
+
+    descripSeleccionada.innerHTML = "Descripción del modelo ";
+
+    precioSeleccionado.innerHTML =  item.getElementsByTagName("span")[0].innerHTML;
+
+
+}
+function cerrar(){
+    mostrador.style.width = "100%";
+    seleccion.style.width = "0%";
+    seleccion.style.opacity = "0";
+    quitarBordes();
+}
+function quitarBordes(){
+    var items = document.getElementsByClassName("item");
+    for(i=0;i <items.length; i++){
+        items[i].style.border = "none";
+    }
+}
+
+
+
+var menuList=document.getElementById("menuList");
+menuList.style.maxHeight="0px";
+
+function togglemenu(){
+    
+    if( menuList.style.maxHeight=="0px")
+    {
+        menuList.style.maxHeight="130px";
+    }
+    else
+    {
+        menuList.style.maxHeight="0px";
+    }
+}
+
+function mostrarPopup() {
+    document.getElementById("miPopup").style.display = "block";
+}
+function ocultarPopup() {
+    document.getElementById("miPopup").style.display = "none";
+}
+ocultarPopup();
